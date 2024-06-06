b411d49116c1bbc21a153a7bf7e135fb54bf9282
Kimberly <kimbilsland@gmail.com>
2024-06-06 10:54:44 -0700

    revise UV functionality to update to live geolocation

--- routes/uvIndexRoutes.js
+++ routes/uvIndexRoutes.js
@@ -6,8 +6,12 @@ const api_Key = "openuv-1a19edrlwgmu11p-io";
 const UVINDEX_URL = `https://api.openuv.io/api/v1/uv`;
 
 router.get("/", async (req, res) => {
-  const lat = 37;
-  const long = -122;
+  const lat = req.query.lat;
+  const long = req.query.long;
+
+  if (!lat || !long) {
+    return res.status(400).json({ message: 'Latitude and longitude are required' });
+  }
 
   try {
     const response = await axios.get(`${UVINDEX_URL}?lat=${lat}&lng=${long}`, {
@@ -16,39 +20,12 @@ router.get("/", async (req, res) => {
       },
     });
     res.json(response.data);
-    console.log(data)
+    console.log(response.data)
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 module.exports = router;
-
-// const express = require("express");
-// const axios = require("axios");
-// const router = express.Router();
-
-// const api_Key = "a642d374f3ea39e8d581f52b4cfde825";
-// const UVINDEX_URL = `https://api.openweathermap.org/data/2.5/uvi`;
-
-// router.get("/api/uvindex", async (req, res) => {
-//   const lat = 37;
-//   const lon = -122;
-
-//   try {
-//     const response = await axios.get(UVINDEX_URL, {
-//       params: {
-//         lat,
-//         lon,
-//         appid: api_Key
-//       }
-//     });
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// module.exports = router;
 
 
