ccf5051da9e3388e2cf25f1e8d9fbf569457d72c
Kimberly <kimbilsland@gmail.com>
2024-06-06 17:47:26 -0700

    add get product by id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,22 @@ router.get('/', async (req, res) => {
       return res.status(500).json({ error: "Internal server error." });
     }
   });
-  
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+    
+        const products = await knex("products").where("id", id).first();
+    
+        if (!products) {
+          return res.status(404).json({ error: "Inventory was not found." });
+        } else {
+          return res.status(200).json(products);
+        }
+      } catch (error) {
+        console.error("Error", error);
+        return res.status(500).json({ error: "Internal server error." });
+      }
+    });
 
 module.exports = router;
