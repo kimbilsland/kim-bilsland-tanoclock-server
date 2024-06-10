a6e14aab5daeba8c8431c0cd909f14645f62e874
Kimberly <kimbilsland@gmail.com>
2024-06-10 13:45:15 -0700

    should be on product featre branch - add seed integration for reviews files

---
+++ seeds/reviews.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Brooke Kellet",
+        content:
+             "Great product! I would buy again.",
+        product_id: 3,
+        rating: 4
+    },
+    {
+        id: 2,
+        name: "Andie Sabiston",
+        content:
+             "Feeling tanned with no sun damage!",
+        product_id: 3,
+        rating: 4
+    }
+]
