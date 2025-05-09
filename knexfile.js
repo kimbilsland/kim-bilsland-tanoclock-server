0f0d7c8f0551f2f42c38941d5b6327c305162f78
Adbloom-MBA <adbloommba@Adbloom-MBAs-MacBook-Air.local>
2025-05-09 17:32:53 -0400

    knex update

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
     client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
@@ -9,6 +8,5 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       charset: "utf8",
-    },
   }
 };
